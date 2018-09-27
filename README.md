f6807e8a37f42d34c34f6d2b8f85791b273030e7
Patrick Ouellet <patrick.ouellet@connexta.com>
2018-09-27 08:32:34 -0700

    MAVEN-1 Preparing for relocation to codice-maven repository

--- README.md
+++ README.md
@@ -3,11 +3,18 @@ Repository of re-usable Maven utilities.
 
 ### Modules
 The following modules are defined:
-* module-1
+* jacoco
  
-#### module-1
-Defines something
+#### jacoco
+Provides extensions for the Jacoco maven plugin. 
+
+[`LenientLimit`](jacoco/src/main/java/org/codice/jacoco/LenientLimit.java) provides an extension to Jacoco's standard limit implementation which is more lenient with respect to the minimum and maximum limits to which a value is compared against. 
+This should help account for small differences in Java compiler and OS as these can be generating different bytecodes.
+
+System properties supported:
+* `offsetJacoco` - specifies the offset to apply to the minimum and maximum values read from pom files in order to be lenient about the comparison. Defaults to 0.02.
+* `computeJacoco` - forces the limit minimum and maximum values as such that Jacoco's comparison will fail and yield warnings that includes the new computed values. This can be useful when one wants to update the pom files after having made changes to the source code or to the test cases.
 
 ### Future iterations
 Future implementations will:
-* do something else
+* Provide CI/CD support
--- jacoco/src/main/java/org/codice/jacoco/LenientLimit.java
+++ jacoco/src/main/java/org/codice/jacoco/LenientLimit.java
@@ -18,29 +18,37 @@ import org.jacoco.report.check.Limit;
 
 /**
  * Extension to Jacoco's {@link Limit} class which is more lenient with respect to the minimum and
- * maximum limits to which a value is compared against. This should help account for small differences
- * in Java compiler and OS as these can be generating different bytecodes.
+ * maximum limits to which a value is compared against. This should help account for small
+ * differences in Java compiler and OS as these can be generating different bytecodes.
  *
  * <p>System properties supported:
+ *
  * <ul>
- *   <li>offsetJacoco - specifies the offset to apply to the minimum and maximum values read from pom
- *   files in order to be lenient about the comparison. Defaults to 0.02.</li>
+ *   <li>offsetJacoco - specifies the offset to apply to the minimum and maximum values read from
+ *       pom files in order to be lenient about the comparison. Defaults to 0.02.
  *   <li>computeJacoco - forces the limit minimum and maximum values as such that Jacoco's
- *   comparison will fail and yield warnings that includes the new computed values. This can be
- *   useful when one wants to update the pom files after having made changes to the source code or
- *   to the test cases.</li>
+ *       comparison will fail and yield warnings that includes the new computed values. This can be
+ *       useful when one wants to update the pom files after having made changes to the source code
+ *       or to the test cases.
  * </ul>
  */
 public class LenientLimit extends Limit {
   private static final boolean COMPUTE = System.getProperties().containsKey("computeJacoco");
-  private static final BigDecimal OFFSET = new BigDecimal(System.getProperty("offsetJacoco", "0.02"));
-  private static final BigDecimal ONE = new BigDecimal("1.00"); // .00 to keep the same scale as jacoco
-  private static final BigDecimal ZERO = new BigDecimal("0.00"); // .00 to keep the same scale as jacoco
+  private static final BigDecimal OFFSET =
+      new BigDecimal(System.getProperty("offsetJacoco", "0.02"));
+  private static final BigDecimal ONE =
+      new BigDecimal("1.00"); // .00 to keep the same scale as jacoco
+  private static final BigDecimal ZERO =
+      new BigDecimal("0.00"); // .00 to keep the same scale as jacoco
 
   @Override
   public void setMinimum(String minimum) {
     if (minimum != null) {
-      minimum = (LenientLimit.COMPUTE ? LenientLimit.ONE : new BigDecimal(minimum).subtract(LenientLimit.OFFSET)).toPlainString();
+      minimum =
+          (LenientLimit.COMPUTE
+                  ? LenientLimit.ONE
+                  : new BigDecimal(minimum).subtract(LenientLimit.OFFSET))
+              .toPlainString();
     }
     super.setMinimum(minimum);
   }
@@ -48,7 +56,11 @@ public class LenientLimit extends Limit {
   @Override
   public void setMaximum(String maximum) {
     if (maximum != null) {
-      maximum = (LenientLimit.COMPUTE ? LenientLimit.ZERO : new BigDecimal(maximum).add(LenientLimit.OFFSET)).toPlainString();
+      maximum =
+          (LenientLimit.COMPUTE
+                  ? LenientLimit.ZERO
+                  : new BigDecimal(maximum).add(LenientLimit.OFFSET))
+              .toPlainString();
     }
     super.setMaximum(maximum);
   }
